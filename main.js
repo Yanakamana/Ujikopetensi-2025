6d18a36ba1462204226d00e59bdcf7d85fd81ef0
yana <tayoempus@gmail.com>
2025-04-28 09:30:20 +0700

    update main.js

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ import {
   orderBy,
   updateDoc
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm9HdJ92vSLrKRclI6Z2J4bmvlFgR4AuU",
+  authDomain: "mang-yana.firebaseapp.com",
+  projectId: "mang-yana",
+  storageBucket: "mang-yana.appspot.com",
+  messagingSenderId: "1094982396668",
+  appId: "1:1094982396668:web:2d103526ab40a59efc0579",
+  measurementId: "G-MF48P7VG5P"
+};
